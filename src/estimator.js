2c925c677c592f338cd507a9aa0e175f1a9984da
Edewor Onyedika <edwordkaso100@gmail.com>
2020-04-14 23:24:15 +0100

    fron-end

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,29 @@
-const covid19ImpactEstimator = (data) => {
-    let impact.currentlyInfected = reportedCases * 10;
-    let severeImpact.currentlyInfected = reportedCases * 50;
+const covid19ImpactEstimator = ({data}) => {
+
+    const input = data;
+
+             // challenge 1
+    let currentlyInfected = data.reportedCases * 10;
+    let severeCurrentlyInfected = data.reportedCases * 50;
 
     let infectionsByRequestedTime = currentlyInfected * 512;
-    let infectionsByRequestedTimeImpact = impact.currentlyInfected * 512;
-    let infectionsByRequestedTimeSevereImpact = severeImpact.currentlyInfected * 512;
+    let infectionsByRequestedTimeSevereImpact = severeCurrentlyInfected * 512;
 
+            // challenge 2
     let severeCasesByRequestedTime = (infectionsByRequestedTime/100) * 15;
     let hospitalBedsByRequestedTime = (totalHospitalBeds/100) * 35;
+
+        // challenge 3
     let casesForICUByRequestedTime = (infectionsByRequestedTime/100) * 5;
     let casesForVentilatorsByRequestedTime = (infectionsByRequestedTime/100) * 2;
 
     let dollarsInFlight = (infectionsByRequestedTime * 0.65 * 1.5) / 30;
+
+    return estimator({
+        data,
+        impact: {},
+        severeImpact: {}
+    });
 };
 
 export default covid19ImpactEstimator;
